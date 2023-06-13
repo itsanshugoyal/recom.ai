c96b423d07b511d5ebbfd9c9b5e3301d6850e712
itsinfinity <itsanshugoyal@gmail.com>
2023-06-13 21:09:28 +0530

    imporved card results

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,65 +1,153 @@
 import React from "react";
 
-function MovieItem({ title, poster_url, trailer_url, overview, vote_average }) {
+function MovieItem({
+  title,
+  genre,
+  poster_url,
+  trailer_url,
+  overview,
+  vote_average,
+}) {
   return (
-    <div className=" max-w-sm lg:w-1/4 xl:w-1/5 m-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      {/* movie image */}
-      <a href="#">
-        <img className=" p-4 rounded-t-lg" src={poster_url} alt="" />
-        {/* <img
-          className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-          src="https://m.media-amazon.com/images/M/MV5BNmQ0ODBhMjUtNDRhOC00MGQzLTk5MTAtZDliODg5NmU5MjZhXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_FMjpg_UX1000_.jpg"
-          alt=""
-        /> */}
-      </a>
-
-      <div className="p-1">
-        {/* imdb ratting */}
-        <div className="flex ">
-          <svg
-            aria-hidden="true"
-            className="w-5 h-5 text-yellow-500"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-          </svg>
-          <p className="mx-2">{vote_average}</p>
+    <div>
+      <div className="group relative">
+        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+          <img
+            src={poster_url}
+            alt={title}
+            className="h-full w-full object-fill object-center lg:h-full lg:w-full"
+          />
         </div>
-        {/* Genre */}
 
-        <p className="mx-2 font-medium text-left">Genre: Magical, Adventure</p>
-        {/* Title */}
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">{title}</h5>
-        </a>
-        {/* movie description */}
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
+        <div className="mt-4 flex justify-between group-hover:opacity-75">
+          <div>
+            <p className="text-sm text-left ml-2 font-medium text-gray-900">
+              {genre}
+            </p>
+          </div>
+          <div className="flex">
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 text-yellow-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <p className=" mx-2 text-sm font-medium text-gray-900">
+              {Math.round(vote_average * 10) / 10}
+            </p>
+          </div>
+        </div>
 
-        {/* any link */}
-        <a
-          href={trailer_url }
-          target="blank"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-700 rounded-lg hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
-        >
-          Trailer
-          <svg
-            aria-hidden="true"
-            className="w-4 h-4 ml-2 -mr-1"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </a>
+        <div className="h-4/6 px-2 max-h-60">
+          <h3 className=" text-gray-700 my-2 font-extrabold text-lg">
+            <a href={trailer_url}>
+              <span aria-hidden="true" className="absolute inset-0 flex" />
+              {title}
+            </a>
+          </h3>
+          {/* {const truncate = _.truncate
+const str = 'The quick brown fox jumps over the lazy dog'
+
+truncate(str, {
+  length: 30, // maximum 30 characters
+  separator: /,?\.* +/ // separate by spaces, including preceding commas and periods
+})
+} */}
+          <p className="mt-1 text-sm text-gray-500">{overview}</p>
+        </div>
+
+        <div className="group-hover:opacity-100">
+          {/* <span className="text-3xl font-bold text-gray-500 tracking-wider leading-relaxed font-sans">
+            Paris city of light
+          </span> */}
+          <div className="pt-8 text-center">
+            {/* any link */}
+            <a
+              href={trailer_url}
+              target="blank"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-700 rounded-lg hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
+            >
+              Trailer
+              <svg
+                aria-hidden="true"
+                className="w-4 h-4 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
+
+    // <div className=" max-w-sm lg:w-1/4 xl:w-1/5 m-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    //   {/* movie image */}
+    //   <a href="#">
+    //     <img className=" p-4 rounded-t-lg" src={poster_url} alt="" />
+    //     {/* <img
+    //       className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+    //       src="https://m.media-amazon.com/images/M/MV5BNmQ0ODBhMjUtNDRhOC00MGQzLTk5MTAtZDliODg5NmU5MjZhXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_FMjpg_UX1000_.jpg"
+    //       alt=""
+    //     /> */}
+    //   </a>
+
+    //   <div className="p-1">
+    //     {/* imdb ratting */}
+    //     <div className="flex ">
+    //       <svg
+    //         aria-hidden="true"
+    //         className="w-5 h-5 text-yellow-500"
+    //         fill="currentColor"
+    //         viewBox="0 0 20 20"
+    //         xmlns="http://www.w3.org/2000/svg"
+    //       >
+    //         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+    //       </svg>
+    //       <p className="mx-2">{vote_average}</p>
+    //     </div>
+    //     {/* Genre */}
+
+    //     <p className="mx-2 font-medium text-left">Genre: {genre}</p>
+    //     {/* Title */}
+    //     <a href="#">
+    //       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">{title}</h5>
+    //     </a>
+    //     {/* movie description */}
+    //     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
+
+    //     {/* any link */}
+    //     <a
+    //       href={trailer_url }
+    //       target="blank"
+    //       className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-700 rounded-lg hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
+    //     >
+    //       Trailer
+    //       <svg
+    //         aria-hidden="true"
+    //         className="w-4 h-4 ml-2 -mr-1"
+    //         fill="currentColor"
+    //         viewBox="0 0 20 20"
+    //         xmlns="http://www.w3.org/2000/svg"
+    //       >
+    //         <path
+    //           fillRule="evenodd"
+    //           d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+    //           clipRule="evenodd"
+    //         ></path>
+    //       </svg>
+    //     </a>
+    //   </div>
+    // </div>
   );
 }
 
--- src/components/MovieResult.jsx
+++ src/components/MovieResult.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import MovieItem from "./MovieItem";
 
-function MovieResult({query}) {
+function MovieResult({ query }) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     query &&
-      fetch(`https://web-production-e62e.up.railway.app/movies/suggest-many?q=${encodeURI( query )}`)
+      fetch(
+        `https://web-production-e62e.up.railway.app/movies/suggest-many?q=${encodeURI(
+          query
+        )}`
+      )
         .then((response) => {
           return response.json();
         })
@@ -16,45 +20,151 @@ function MovieResult({query}) {
   }, [query]);
   console.log(data);
 
-  /*   const data = [
-      {
-        adult: false,
-        backdrop_path: "/ecKQlAEG95k62SMGhvX83oEqANK.jpg",
-        genre_ids: [27, 53],
-        id: 1,
-        original_language: "en",
-        original_title: "The Conjuring",
-        overview:
-          "Paranormal investigators Ed and Lorraine Warren work to help a family terrorized by a dark presence in their farmhouse. Forced to confront a powerful entity, the Warrens find themselves caught in the most terrifying case of their lives.",
-        popularity: 103.574,
-        poster_path: "/wVYREutTvI2tmxr6ujrHT704wGF.jpg",
-        release_date: "2013-07-18",
-        title: "The Conjuring",
-        video: false,
-        vote_average: 7.539,
-        vote_count: 10352,
-        trailer_url: "https://www.youtube.com/watch?v=ejMMn0t58Lc",
-        poster_url: "https://image.tmdb.org/t/p/original//wVYREutTvI2tmxr6ujrHT704wGF.jpg",
-      },
+    // const data = [
+    //   {
+
+    //     adult: false,
+    //     backdrop_path: "/mmd1HnuvAzFc4iuVJcnBrhDNEKr.jpg",
+    //     genre_ids: [27, 53],
+    //     genre_names: ['Horror', 'Thriller'],
+    //     id: 2,
+    //     original_language: "en",
+    //     original_title: "The Shining",
+    //     overview: "Jack Torrance accepts a caretaker job at the Overlook Hotel, where he, along with his wife Wendy and their son Danny, must live isolated from the rest of the world for the winter. But they aren't prepared for the madness that lurks within.",
+    //     popularity: 41.038,
+    //     poster_path: "/nRj5511mZdTl4saWEPoj9QroTIu.jpg",
+    //     poster_url: "https://image.tmdb.org/t/p/original//nRj5511mZdTl4saWEPoj9QroTIu.jpg",
+    //     release_date: "1980-05-23",
+    //     title: "The Shining",
+    //     trailer_url: "https://www.youtube.com/watch?v=5Cb3ik6zP2I",
+    //     video: false,
+    //     vote_average: 8.22,
+    //     vote_count: 15783,
+
+    //   },{
+
+    //     adult: false,
+    //     backdrop_path: "/mmd1HnuvAzFc4iuVJcnBrhDNEKr.jpg",
+    //     genre_ids: [27, 53],
+    //     genre_names: ['Horror', 'Thriller'],
+    //     id: 3,
+    //     original_language: "en",
+    //     original_title: "The Shining",
+    //     overview: "Jack Torrance accepts a caretaker job at the Overlook Hotel, where he, along with his wife Wendy and their son Danny, must live isolated from the rest of the world for the winter. But they aren't prepared for the madness that lurks within.",
+    //     popularity: 41.038,
+    //     poster_path: "/nRj5511mZdTl4saWEPoj9QroTIu.jpg",
+    //     poster_url: "https://image.tmdb.org/t/p/original//nRj5511mZdTl4saWEPoj9QroTIu.jpg",
+    //     release_date: "1980-05-23",
+    //     title: "The Shining",
+    //     trailer_url: "https://www.youtube.com/watch?v=5Cb3ik6zP2I",
+    //     video: false,
+    //     vote_average: 8.22,
+    //     vote_count: 15783,
+
+    //   },{
+
+    //     adult: false,
+    //     backdrop_path: "/mmd1HnuvAzFc4iuVJcnBrhDNEKr.jpg",
+    //     genre_ids: [27, 53],
+    //     genre_names: ['Horror', 'Thriller'],
+    //     id: 4,
+    //     original_language: "en",
+    //     original_title: "The Shining",
+    //     overview: "Jack Torrance accepts a caretaker job at the Overlook Hotel, where he, along with his wife Wendy and their son Danny, must live isolated from the rest of the world for the winter. But they aren't prepared for the madness that lurks within.",
+    //     popularity: 41.038,
+    //     poster_path: "/nRj5511mZdTl4saWEPoj9QroTIu.jpg",
+    //     poster_url: "https://image.tmdb.org/t/p/original//nRj5511mZdTl4saWEPoj9QroTIu.jpg",
+    //     release_date: "1980-05-23",
+    //     title: "The Shining",
+    //     trailer_url: "https://www.youtube.com/watch?v=5Cb3ik6zP2I",
+    //     video: false,
+    //     vote_average: 8.22,
+    //     vote_count: 15783,
+
+    //   },{
+
+    //     adult: false,
+    //     backdrop_path: "/mmd1HnuvAzFc4iuVJcnBrhDNEKr.jpg",
+    //     genre_ids: [27, 53],
+    //     genre_names: ['Horror', 'Thriller'],
+    //     id: 6,
+    //     original_language: "en",
+    //     original_title: "The Shining",
+    //     overview: "Jack Torrance accepts a caretaker job at the Overlook Hotel, where he, along with his wife Wendy and their son Danny, must live isolated from the rest of the world for the winter. But they aren't prepared for the madness that lurks within.",
+    //     popularity: 41.038,
+    //     poster_path: "/nRj5511mZdTl4saWEPoj9QroTIu.jpg",
+    //     poster_url: "https://image.tmdb.org/t/p/original//nRj5511mZdTl4saWEPoj9QroTIu.jpg",
+    //     release_date: "1980-05-23",
+    //     title: "The Shining",
+    //     trailer_url: "https://www.youtube.com/watch?v=5Cb3ik6zP2I",
+    //     video: false,
+    //     vote_average: 8.22,
+    //     vote_count: 15783,
+
+    //   },{
+
+    //     adult: false,
+    //     backdrop_path: "/mmd1HnuvAzFc4iuVJcnBrhDNEKr.jpg",
+    //     genre_ids: [27, 53],
+    //     genre_names: ['Horror', 'Thriller'],
+    //     id: 8,
+    //     original_language: "en",
+    //     original_title: "The Shining",
+    //     overview: "Jack Torrance accepts a caretaker job at the Overlook Hotel, where he, along with his wife Wendy and their son Danny, must live isolated from the rest of the world for the winter. But they aren't prepared for the madness that lurks within.",
+    //     popularity: 41.038,
+    //     poster_path: "/nRj5511mZdTl4saWEPoj9QroTIu.jpg",
+    //     poster_url: "https://image.tmdb.org/t/p/original//nRj5511mZdTl4saWEPoj9QroTIu.jpg",
+    //     release_date: "1980-05-23",
+    //     title: "The Shining",
+    //     trailer_url: "https://www.youtube.com/watch?v=5Cb3ik6zP2I",
+    //     video: false,
+    //     vote_average: 8.22,
+    //     vote_count: 15783,
+
+    //   },
      
-    ]; */
+    // ];
+
 
   return (
-    <div>
-      <div className="container flex-wrap flex justify-around max-w-full ">
-        {data.length>0 && data.map((moviedata) => {
-          return (
-            <MovieItem
-              key={moviedata.id}
-              title={moviedata.title}
-              poster_url={moviedata.poster_url}
-              trailer_url={moviedata.trailer_url}
-              vote_average={moviedata.vote_average}
-              overview={moviedata.overview}
-            />
-          );
-        })}
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          
+            {data.length > 0 &&
+              data.map((moviedata) => {
+                return (
+                  <MovieItem
+                    key={moviedata.id}
+                    title={moviedata.title}
+                    genre={moviedata.genre_names.toString()}
+                    poster_url={moviedata.poster_url}
+                    trailer_url={moviedata.trailer_url}
+                    vote_average={moviedata.vote_average}
+                    overview={moviedata.overview}
+                  />
+                );
+              })}
+          
+        </div>
       </div>
+
+      {/* <div className="container flex-wrap flex justify-around max-w-full ">
+        {data.length > 0 &&
+          data.map((moviedata) => {
+            return (
+              <MovieItem
+                key={moviedata.id}
+                title={moviedata.title}
+                genre={moviedata.genre_names.toString()}
+                poster_url={moviedata.poster_url}
+                trailer_url={moviedata.trailer_url}
+                vote_average={moviedata.vote_average}
+                overview={moviedata.overview}
+              />
+            );
+          })}
+      </div> */}
     </div>
   );
 }
